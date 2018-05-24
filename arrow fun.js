bfd46ba1a298589cc3f9def456b06710e1d00462
rennyvick <irenevky85@gmail.com>
2018-05-24 17:22:30 +0300

    my arrow commit

---
+++ arrow fun.js
@@ -0,0 +1,25 @@
+function concat(word1, word2, word3) {
+return a + b + c;
+ 
+}
+
+ let a ="Jane",
+ b = "Doe" ,
+ c = 19;
+ let student = concat(a + b +c);
+ console.log(student)
+
+
+
+ function concat(word1 , word2 , word3 ) {
+ const space =" ";
+return  a + space+ b + space + c ;
+ 
+}
+
+ let a = "jane";
+ b = "Doe"; 
+ c = 19;
+ 
+ let student = concat (a ,b ,c);
+ console.log(student)
---
+++ promises.js
@@ -0,0 +1,6 @@
+setTimeout(function(){
+	console.log('5 seconds have elapsed')
+}, 5000);
+
+
+console.log('finished');
